834b39e5cfc46fce32d809c60841b9c5222ee031
unknown <jdnyc84@gmail.com>
2020-07-05 14:39:59 -0400

    javascript jumping ball game

---
+++ jump.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const dino = document.querySelector('.dino');
+  const grid = document.querySelector('.grid');
+  const alert = document.getElementById('alert');
+  let isJumping = false;
+  let gravity = 0.9;
+  let position = 0;
+  let isGameOver = false;
+
+  function control(e) {
+    // make dino jump, keyCode 32 is spacebar key
+    if (e.keyCode === 32) {
+      if (!isJumping) {
+        isJumping = true;
+        jump();
+      }
+    }
+  }
+
+  document.addEventListener('keydown', control);
+
+  function jump() {
+    let count = 0;
+    let timerId = setInterval(function () {
+      // stop it and move it down
+      if (count === 15) {
+        clearInterval(timerId); // stops the up functionality from running
+        let downTimerId = setInterval(function () {
+          if (count === 0) {
+            clearInterval(downTimerId);
+            isJumping = false;
+          }
+          position -= 5;
+          count--;
+          position = position * gravity;
+          dino.style.bottom = position + 'px';
+        }, 20);
+      }
+
+      //   moveup
+
+      position += 30;
+      count++;
+      position = position * gravity;
+      dino.style.bottom = position + 'px';
+    }, 20);
+  }
+
+  function generateObstacles() {
+    // creates the obstacle to jump over
+    let randomTime = Math.random() * 4000;
+    let obstaclePosition = 1000;
+    const obstacle = document.createElement('div');
+    if (!isGameOver) obstacle.classList.add('obstacle');
+    grid.appendChild(obstacle);
+    obstacle.style.left = obstaclePosition + 'px';
+    // moves the obstacle
+    let timerId = setInterval(function () {
+      if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+        clearInterval(timerId);
+        alert.innerHTML = 'Game Over';
+        isGameOver = true;
+        // remove all child divs
+        while (grid.firstChild) {
+            grid.removeChild(grid.lastChild)
+        }
+      }
+      obstaclePosition -= 10;
+      obstacle.style.left = obstaclePosition + 'px';
+    }, 20);
+    if (!isGameOver) setTimeout(generateObstacles, randomTime);
+  }
+
+  generateObstacles();
+});
